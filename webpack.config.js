62ec00b63dafff1bddd79659e1b4bfe19c676961
Jesús Ferretti <jferrettiboke@gmail.com>
2016-04-15 15:56:30 +0200

    Simplify the Webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,44 @@
+var fs = require('fs');
 var path = require('path');
 var webpack = require('webpack');
 
-module.exports = {
-  entry: './src/client/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist/public'),
-    filename: 'bundle.js'
+var commonLoaders = [
+  { test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/ }
+];
+
+module.exports = [
+  {
+    // The configuration for the client-side rendering
+    entry: path.resolve(__dirname, './src/client/index.js'),
+    output: {
+      path: path.resolve(__dirname, 'dist/public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      loaders: commonLoaders
+    }
   },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        loader: 'style!css'
-      }
-    ]
+  {
+    // The configuration for the server-side rendering
+    entry: path.resolve(__dirname, './src/server/index.js'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'server.js'
+    },
+    target: 'node',
+    // keep node_module paths out of the bundle
+    externals: fs.readdirSync(path.resolve(__dirname, 'node_modules')).concat([
+      'react-dom/server'
+    ]).reduce(function (ext, mod) {
+      ext[mod] = 'commonjs ' + mod
+      return ext
+    }, {}),
+    node: {
+      __filename: true,
+      __dirname: true
+    },
+    module: {
+      loaders: commonLoaders
+    }
   }
-};
+];

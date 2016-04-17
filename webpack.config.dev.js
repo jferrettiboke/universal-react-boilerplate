32859587a288e925047e17861094138a0383f231
Jesús Ferretti <jferrettiboke@gmail.com>
2016-04-17 22:44:25 +0200

    Add React Hot Loader

---
+++ webpack.config.dev.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/client/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist/public'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src'),
+      exclude: /node_modules/
+    }]
+  }
+};

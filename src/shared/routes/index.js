2d1c474d9546d4e0ed76269e6797c7237061743d
Jesús Ferretti <jferrettiboke@gmail.com>
2016-06-15 00:17:53 +0200

    Organize components inside folders

--- src/shared/routes/index.js
+++ src/shared/routes/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import App from '../components/App';
-import Landing from '../components/Landing';
-import About from '../components/About';
+import App from '../components/App/App';
+import Landing from '../components/Landing/Landing';
+import About from '../components/About/About';
 
 export default (
   <Route path="/" component={App}>
